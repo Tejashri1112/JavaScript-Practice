f48cd0396a3b336e6a34191f90f461074365dd35
Tejashri Takale <143163785+Tejashri1112@users.noreply.github.com>
2024-10-05 23:14:01 +0530

    Add files via upload

---
+++ Conditional_stat.js
@@ -0,0 +1,34 @@
+//if
+
+let a=10;
+
+if(a==10)
+{
+    console.log(a);
+}
+
+// if-else
+
+if(a==10)
+{
+    console.log(a);
+}
+else
+{
+    console.log("false");
+}
+
+// if-elseif-else
+
+if(a>10)
+{
+    console.log("a is greater than 10");
+}
+else if(a<10)
+{
+    console.log("a is less than 10");
+}
+else
+{
+    console.log("a is equal to 10");
+}
