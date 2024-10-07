213e1b852ab910eaa56b41d8f36d4155e6399ebc
Tejashri Takale <143163785+Tejashri1112@users.noreply.github.com>
2024-10-07 23:41:47 +0530

    Add files via upload

---
+++ Lec4_loops.js
@@ -0,0 +1,25 @@
+// for loop
+
+for(let i=1;i<5;i++)
+{
+    console.log(i);
+}
+
+//while loop
+let a=1;
+while(a<=5)
+{
+console.log(a);
+a++;
+}
+
+// do -while
+do{
+    console.log(a);
+    a++;
+}while(a<=5)
+
+
+    // for of loop --- it is used for string and array
+
+    // for in loop----used for object
